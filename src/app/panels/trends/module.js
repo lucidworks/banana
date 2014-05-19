3666957062d199f053a2a406694118f750346ef8
Tarek Salah <tarek.salah@badrit.com>
2014-05-19 10:56:01 +0300

    convert one solr query to 2 solr query

--- src/app/panels/trends/module.js
+++ src/app/panels/trends/module.js
@@ -6,65 +6,67 @@
   * style :: A hash of css styles
   * arrangement :: How should I arrange the query results? 'horizontal' or 'vertical'
   * ago :: Date math formatted time to look back
-
 */
 define([
-  'angular',
-  'app',
-  'underscore',
-  'kbn'
-],
-function (angular, app, _, kbn) {
-  'use strict';
+    'angular',
+    'app',
+    'underscore',
+    'kbn'
+  ],
+  function(angular, app, _, kbn) {
+    'use strict';
 
-  var module = angular.module('kibana.panels.trends', []);
-  app.useModule(module);
+    var module = angular.module('kibana.panels.trends', []);
+    app.useModule(module);
 
-  var DEBUG = true;
+    var DEBUG = true;
 
-  module.controller('trends', function($scope, kbnIndex, querySrv, dashboard, filterSrv) {
+    module.controller('trends', function($scope, kbnIndex, querySrv, dashboard, filterSrv) {
 
-    $scope.panelMeta = {
-      modals : [
-        {
+      $scope.panelMeta = {
+        modals: [{
           description: "Inspect",
           icon: "icon-info-sign",
           partial: "app/partials/inspector.html",
           show: $scope.panel.spyable
-        }
-      ],
-      editorTabs : [
-        {title:'Queries', src:'app/partials/querySelect.html'}
-      ],
-      status  : "Beta",
-      description : "A stock-ticker style representation of how queries are moving over time. "+
-      "For example, if the time is 1:10pm, your time picker was set to \"Last 10m\", and the \"Time "+
-      "Ago\" parameter was set to '1h', the panel would show how much the query results have changed"+
-      " since 12:00-12:10pm"
-    };
+        }],
+        editorTabs: [{
+          title: 'Queries',
+          src: 'app/partials/querySelect.html'
+        }],
+        status: "Beta",
+        description: "A stock-ticker style representation of how queries are moving over time. " +
+          "For example, if the time is 1:10pm, your time picker was set to \"Last 10m\", and the \"Time " +
+          "Ago\" parameter was set to '1h', the panel would show how much the query results have changed" +
+          " since 12:00-12:10pm"
+      };
 
-    // Set and populate defaults
-    var _d = {
-      queries     : {
-        mode        : 'all',
-        ids         : []
-      },
-      style   : { "font-size": '14pt'},
-      ago     : '1d',
-      arrangement : 'vertical',
-      spyable: true
-    };
-    _.defaults($scope.panel,_d);
+      // Set and populate defaults
+      var _d = {
+        queries: {
+          mode: 'all',
+          ids: []
+        },
+        style: {
+          "font-size": '14pt'
+        },
+        ago: '1d',
+        arrangement: 'vertical',
+        spyable: true
+      };
+      _.defaults($scope.panel, _d);
 
-    $scope.init = function () {
-      $scope.hits = 0;
+      $scope.init = function() {
+        $scope.hits = 0;
 
-      $scope.$on('refresh', function(){$scope.get_data();});
+        $scope.$on('refresh', function() {
+          $scope.get_data();
+        });
 
-      $scope.get_data();
-    };
+        $scope.get_data();
+      };
 
-    $scope.get_data = function(segment, query_id) {
+      $scope.get_data = function(segment, query_id) {
         delete $scope.panel.error;
         $scope.panelMeta.loading = true;
 
@@ -162,154 +164,174 @@ function (angular, app, _, kbn) {
         // current time
         // make the gap equal to the difference between the start and end date
         // this will help in reducing response size 
-        var facet_date_gap = '%2B' + diffDays($scope.time.from, $scope.time.to) + 'DAY';
-        var facet_date = '&facet=true' +
-          '&facet.date=' + time_field +
-          '&facet.date.start=' + $scope.time.from.toISOString() +
-          '&facet.date.end=' + $scope.time.to.toISOString() +
-          '&facet.date.gap=' + facet_date_gap +
-          '&facet.date.other=between';
+        var facet_first_gap = '%2B' + diffDays($scope.time.from, $scope.time.to) + 'DAY';
+        var facet_first_range = '&facet=true' +
+          '&facet.range=' + time_field +
+          '&facet.range.start=' + $scope.time.from.toISOString() +
+          '&facet.range.end=' + $scope.time.to.toISOString() +
+          '&facet.range.gap=' + facet_first_gap +
+          '&facet.range.other=between';
 
         // time ago
-        var facet_range_gap = '%2B' + diffDays($scope.old_time.from, $scope.old_time.to) + 'DAY';
-        var facet_range =
+        var facet_second_gap = '%2B' + diffDays($scope.old_time.from, $scope.old_time.to) + 'DAY';
+        var facet_second_range = '&facet=true' +
           '&facet.range=' + time_field +
           '&facet.range.start=' + $scope.old_time.from.toISOString() +
           '&facet.range.end=' + $scope.old_time.to.toISOString() +
-          '&facet.range.gap=' + facet_range_gap +
+          '&facet.range.gap=' + facet_second_gap +
           '&facet.range.other=between';
 
-        $scope.panel.queries.query = querySrv.getQuery(0) + wt_json + rows_limit + facet_date + facet_range;
-        request = request.setQuery($scope.panel.queries.query);
-        var results = request.doSearch();
-        results.then(function(results) {
-          if (DEBUG) {
-            console.log('new time')
-            console.log($scope.time.from, $scope.time.to)
-            console.log('old time')
-            console.log($scope.old_time.from, $scope.old_time.to)
-            console.log(results)
-          }
-          processSolrResults(results);
-          $scope.$emit('render');
+        var first_request = querySrv.getQuery(0) + wt_json + rows_limit + facet_first_range;
+        var second_request = querySrv.getQuery(0) + wt_json + rows_limit + facet_second_range;
+        $scope.panel.queries.query = first_request + "\n" + second_request;
+
+        request = request.setQuery(first_request);
+        var results_new = request.doSearch();
+
+        results_new.then(function(results_new) {
+          // Second Query
+          request = request.setQuery(second_request);
+          var results_old = request.doSearch();
+
+          results_old.then(function(results_old) {
+            if (DEBUG) {
+              console.log('new time')
+              console.log($scope.time.from, $scope.time.to)
+              console.log('old time')
+              console.log($scope.old_time.from, $scope.old_time.to)
+              console.log(results_new)
+              console.log(results_old)
+            }
+            processSolrResults(results_new, results_old);
+            $scope.$emit('render');
+          });
         });
 
       };
 
-    // Populate scope when we have results
-    var process_results = function(results,_segment,query_id) {
-      results.then(function(results) {
+      // Populate scope when we have results
+      var process_results = function(results, _segment, query_id) {
+        results.then(function(results) {
+          $scope.panelMeta.loading = false;
+          if (_segment === 0) {
+            $scope.hits = {};
+            $scope.data = [];
+            query_id = $scope.query_id = new Date().getTime();
+          }
+
+          // Check for error and abort if found
+          if (!(_.isUndefined(results.error))) {
+            $scope.panel.error = $scope.parse_error(results.error.msg);
+            return;
+          }
+
+          // Convert facet ids to numbers
+          var facetIds = _.map(_.keys(results.facets), function(k) {
+            if (!isNaN(k)) {
+              return parseInt(k, 10);
+            }
+          });
+
+          // Make sure we're still on the same query/queries
+          if ($scope.query_id === query_id &&
+            _.intersection(facetIds, $scope.panel.queries.ids).length === $scope.panel.queries.ids.length
+          ) {
+            var i = 0;
+            _.each($scope.panel.queries.ids, function(id) {
+              var n = results.facets[id].count;
+              var o = results.facets['old_' + id].count;
+
+              var hits = {
+                new: _.isUndefined($scope.data[i]) || _segment === 0 ? n : $scope.data[i].hits.new + n,
+                old: _.isUndefined($scope.data[i]) || _segment === 0 ? o : $scope.data[i].hits.old + o
+              };
+
+              $scope.hits.new += n;
+              $scope.hits.old += o;
+
+              var percent = percentage(hits.old, hits.new) == null ?
+                '?' : Math.round(percentage(hits.old, hits.new) * 100) / 100;
+              // Create series
+              $scope.data[i] = {
+                info: querySrv.list[id],
+                hits: {
+                  new: hits.new,
+                  old: hits.old
+                },
+                percent: percent
+              };
+
+              i++;
+            });
+            $scope.$emit('render');
+            if (_segment < $scope.index.length - 1) {
+              $scope.get_data(_segment + 1, query_id);
+            } else {
+              $scope.trends = $scope.data;
+            }
+          }
+        });
+      };
+
+
+      function processSolrResults(results_new, results_old) {
         $scope.panelMeta.loading = false;
-        if(_segment === 0) {
-          $scope.hits = {};
-          $scope.data = [];
-          query_id = $scope.query_id = new Date().getTime();
-        }
 
         // Check for error and abort if found
-        if(!(_.isUndefined(results.error))) {
-          $scope.panel.error = $scope.parse_error(results.error);
+        if (!(_.isUndefined(results_new.error))) {
+          $scope.panel.error = results_new.error.msg;
           return;
         }
 
-        // Convert facet ids to numbers
-        var facetIds = _.map(_.keys(results.facets),function(k){if(!isNaN(k)){return parseInt(k, 10);}});
-
-        // Make sure we're still on the same query/queries
-        if($scope.query_id === query_id &&
-          _.intersection(facetIds,$scope.panel.queries.ids).length === $scope.panel.queries.ids.length
-          ) {
-          var i = 0;
-          _.each($scope.panel.queries.ids, function(id) {
-            var n = results.facets[id].count;
-            var o = results.facets['old_'+id].count;
-
-            var hits = {
-              new : _.isUndefined($scope.data[i]) || _segment === 0 ? n : $scope.data[i].hits.new+n,
-              old : _.isUndefined($scope.data[i]) || _segment === 0 ? o : $scope.data[i].hits.old+o
-            };
-
-            $scope.hits.new += n;
-            $scope.hits.old += o;
-
-            var percent = percentage(hits.old,hits.new) == null ?
-              '?' : Math.round(percentage(hits.old,hits.new)*100)/100;
-            // Create series
-            $scope.data[i] = {
-              info: querySrv.list[id],
-              hits: {
-                new : hits.new,
-                old : hits.old
-              },
-              percent: percent
-            };
-
-            i++;
-          });
-          $scope.$emit('render');
-          if(_segment < $scope.index.length-1) {
-            $scope.get_data(_segment+1,query_id);
-          } else {
-            $scope.trends = $scope.data;
-          }
+        if (!(_.isUndefined(results_old.error))) {
+          $scope.panel.error = results_old.error.msg;
+          return;
         }
-      });
-    };
 
+        $scope.hits = {};
+        $scope.data = [];
 
-    function processSolrResults(results) {
-      $scope.panelMeta.loading = false;
+        var hits = {
+          new: results_new.facet_counts.facet_ranges[filterSrv.getTimeField()]['between'],
+          old: results_old.facet_counts.facet_ranges[filterSrv.getTimeField()]['between']
+        }
+        $scope.hits = hits;
 
-      // Check for error and abort if found
-      if (!(_.isUndefined(results.error))) {
-        $scope.panel.error = results.error.msg;
-        return;
+        var percent = percentage(hits.old, hits.new) == null ?
+          '?' : Math.round(percentage(hits.old, hits.new) * 100) / 100;
+        // Create series
+        $scope.data[0] = {
+          info: querySrv.list[0],
+          hits: {
+            new: hits.new,
+            old: hits.old
+          },
+          percent: percent
+        };
+        $scope.trends = $scope.data;
       }
 
-      $scope.hits = {};
-      $scope.data = [];
-
-      var hits = {
-        new: results.facet_counts.facet_dates[filterSrv.getTimeField()]['between'],
-        old: results.facet_counts.facet_ranges[filterSrv.getTimeField()]['between']
+      function diffDays(date1, date2) {
+        // calculate the number of days between two dates
+        var oneDay = 24 * 60 * 60 * 1000;
+        return (Math.round(Math.abs((date2.getTime() - date1.getTime()) / (oneDay))) + 1);
       }
-      $scope.hits = hits;
 
-      var percent = percentage(hits.old, hits.new) == null ?
-        '?' : Math.round(percentage(hits.old, hits.new) * 100) / 100;
-      // Create series
-      $scope.data[0] = {
-        info: querySrv.list[0],
-        hits: {
-          new: hits.new,
-          old: hits.old
-        },
-        percent: percent
+      function percentage(x, y) {
+        return x === 0 ? null : 100 * (y - x) / x;
+      }
+
+      $scope.set_refresh = function(state) {
+        $scope.refresh = state;
       };
-      $scope.trends = $scope.data;
-    }
 
-    function diffDays(date1, date2) {
-      // calculate the number of days between two dates
-      var oneDay = 24 * 60 * 60 * 1000;
-      return (Math.round(Math.abs((date2.getTime() - date1.getTime()) / (oneDay))) + 1);
-    }
+      $scope.close_edit = function() {
+        if ($scope.refresh) {
+          $scope.get_data();
+        }
+        $scope.refresh = false;
+        $scope.$emit('render');
+      };
 
-    function percentage(x,y) {
-      return x === 0 ? null : 100*(y-x)/x;
-    }
-
-    $scope.set_refresh = function (state) {
-      $scope.refresh = state;
-    };
-
-    $scope.close_edit = function() {
-      if($scope.refresh) {
-        $scope.get_data();
-      }
-      $scope.refresh =  false;
-      $scope.$emit('render');
-    };
-
+    });
   });
-});
