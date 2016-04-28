8817172582ddfc3bc4708d305b0fc7203b1ba5df
Andrew T <andrew.thana@gmail.com>
2016-04-28 10:11:01 +0700

    Clean up code.

--- src/app/components/require.config.js
+++ src/app/components/require.config.js
@@ -43,7 +43,7 @@ require.config({
     modernizr:                '../vendor/modernizr-2.6.1',
     elasticjs:                '../vendor/elasticjs/elastic-angular-client',
     solrjs:                   '../vendor/solrjs/solr-angular-client',
-    d3:                       '../vendor/d3',
+    d3:                       '../vendor/d3'
   },
   shim: {
     underscore: {
--- src/app/controllers/dash.js
+++ src/app/controllers/dash.js
@@ -1,91 +1,90 @@
 define([
-  'angular',
-  'config',
-  'underscore',
-  'services/all'
+    'angular',
+    'config',
+    'underscore',
+    'services/all'
 ],
 function (angular, config, _) {
-  "use strict";
+    "use strict";
 
-  var module = angular.module('kibana.controllers');
+    var module = angular.module('kibana.controllers');
 
-  module.controller('DashCtrl', function(
-    $scope, $route, ejsResource, sjsResource, fields, dashboard, alertSrv, panelMove) {
-    $scope.editor = {
-      index: 0
-    };
+    module.controller('DashCtrl', function ($scope, $route, ejsResource, sjsResource, fields, dashboard, alertSrv, panelMove) {
+        $scope.editor = {
+            index: 0
+        };
 
-    // For moving stuff around the dashboard. Needs better names
-    $scope.panelMove = panelMove;
-    $scope.panelMoveDrop = panelMove.onDrop;
-    $scope.panelMoveStart = panelMove.onStart;
-    $scope.panelMoveStop = panelMove.onStop;
-    $scope.panelMoveOver = panelMove.onOver;
-    $scope.panelMoveOut = panelMove.onOut;
+        // For moving stuff around the dashboard. Needs better names
+        $scope.panelMove = panelMove;
+        $scope.panelMoveDrop = panelMove.onDrop;
+        $scope.panelMoveStart = panelMove.onStart;
+        $scope.panelMoveStop = panelMove.onStop;
+        $scope.panelMoveOver = panelMove.onOver;
+        $scope.panelMoveOut = panelMove.onOut;
 
-    $scope.init = function() {
-      $scope.config = config;
-      // Make underscore.js available to views
-      $scope._ = _;
-      $scope.dashboard = dashboard;
-      $scope.dashAlerts = alertSrv;
-      alertSrv.clearAll();
+        $scope.init = function () {
+            $scope.config = config;
+            // Make underscore.js available to views
+            $scope._ = _;
+            $scope.dashboard = dashboard;
+            $scope.dashAlerts = alertSrv;
+            alertSrv.clearAll();
 
-      // Provide a global list of all see fields
-      $scope.fields = fields;
-      $scope.reset_row();
+            // Provide a global list of all see fields
+            $scope.fields = fields;
+            $scope.reset_row();
 
-      // Solr
-      $scope.ejs = ejsResource(config.elasticsearch);
-      $scope.sjs = sjsResource(config.solr + config.solr_core);
-    };
+            // Solr
+            $scope.ejs = ejsResource(config.elasticsearch);
+            $scope.sjs = sjsResource(config.solr + config.solr_core);
+        };
 
-    $scope.isPanel = function(obj) {
-      if(!_.isNull(obj) && !_.isUndefined(obj) && !_.isUndefined(obj.type)) {
-        return true;
-      } else {
-        return false;
-      }
-    };
+        $scope.isPanel = function (obj) {
+            if (!_.isNull(obj) && !_.isUndefined(obj) && !_.isUndefined(obj.type)) {
+                return true;
+            } else {
+                return false;
+            }
+        };
 
-    $scope.add_row = function(dash,row) {
-      dash.rows.push(row);
-    };
+        $scope.add_row = function (dash, row) {
+            dash.rows.push(row);
+        };
 
-    $scope.reset_row = function() {
-      $scope.row = {
-        title: '',
-        height: '150px',
-        editable: true,
-      };
-    };
+        $scope.reset_row = function () {
+            $scope.row = {
+                title: '',
+                height: '150px',
+                editable: true,
+            };
+        };
 
-    $scope.row_style = function(row) {
-      return { 'min-height': row.collapse ? '5px' : row.height };
-    };
+        $scope.row_style = function (row) {
+            return {'min-height': row.collapse ? '5px' : row.height};
+        };
 
-    $scope.edit_path = function(type) {
-      if(type) {
-        return 'app/panels/'+type+'/editor.html';
-      } else {
-        return false;
-      }
-    };
+        $scope.edit_path = function (type) {
+            if (type) {
+                return 'app/panels/' + type + '/editor.html';
+            } else {
+                return false;
+            }
+        };
 
-    $scope.setEditorTabs = function(panelMeta) {
-      $scope.editorTabs = ['General','Panel','Info'];
-      if(!_.isUndefined(panelMeta.editorTabs)) {
-        $scope.editorTabs =  _.union($scope.editorTabs,_.pluck(panelMeta.editorTabs,'title'));
-      }
-      return $scope.editorTabs;
-    };
+        $scope.setEditorTabs = function (panelMeta) {
+            $scope.editorTabs = ['General', 'Panel', 'Info'];
+            if (!_.isUndefined(panelMeta.editorTabs)) {
+                $scope.editorTabs = _.union($scope.editorTabs, _.pluck(panelMeta.editorTabs, 'title'));
+            }
+            return $scope.editorTabs;
+        };
 
-    // This is whoafully incomplete, but will do for now
-    $scope.parse_error = function(data) {
-      var _error = data.match("nested: (.*?);");
-      return _.isNull(_error) ? data : _error[1];
-    };
+        // This is whoafully incomplete, but will do for now
+        $scope.parse_error = function (data) {
+            var _error = data.match("nested: (.*?);");
+            return _.isNull(_error) ? data : _error[1];
+        };
 
-    $scope.init();
-  });
+        $scope.init();
+    });
 });
