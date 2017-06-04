009824bf7b8785bd7b128704acf9846ca00c705e
Andrew Thanalertvisuti <andrew.thana@gmail.com>
2017-06-04 15:00:53 +0700

    Remove dist directory from the repo and update .gitignore dist directory is created by running 'grunt build' command.

--- dist/vendor/require/tmpl.js
+++
@@ -1,5 +0,0 @@
-/*! banana-fusion - v1.6.17 - 2017-06-01
- * https://github.com/LucidWorks/banana/wiki
- * Copyright (c) 2017 Andrew Thanalertvisuti; Licensed Apache-2.0 */
-
-define(["module"],function(a){"use strict";var b=a.config&&a.config()||{};return{load:function(a,c,d,e){var f=c.toUrl(a);c(["text!"+a],function(a){b.registerTemplate&&b.registerTemplate(f,a),d(a)})}}});
