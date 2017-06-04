f44249193191ef125681c0d49267ad39cb5b611b
Andrew Thanalertvisuti <andrew.thana@gmail.com>
2017-06-04 20:30:21 +0700

    Fix bug for Solr when saving and loading dashboard from the dashboard field defined as a multi-valued field. Rename index.html title from "Banana 3" to "Banana"

--- src/app/controllers/dashLoader.js
+++ src/app/controllers/dashLoader.js
@@ -325,7 +325,12 @@ function (angular, _, config) {
                   doc.server = '';
                 } else {
                   doc.id = dashboardList[i].id;
-                  doc.server = angular.fromJson(dashboardList[i][self.DASHBOARD_FIELD]).solr.server;
+                  // Handle a case where the dashboard field is a multi-valued field (array).
+                  if (dashboardList[i][self.DASHBOARD_FIELD] instanceof Array) {
+                    doc.server = angular.fromJson(dashboardList[i][self.DASHBOARD_FIELD][0]).solr.server;
+                  } else {
+                    doc.server = angular.fromJson(dashboardList[i][self.DASHBOARD_FIELD]).solr.server;
+                  }
                 }
                 docs.push(doc);
             }
