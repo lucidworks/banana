5f338404b7172a1f7ca46dcbdd5fa464066ceb3b
Andrew Thanalertvisuti <andrew.thana@gmail.com>
2018-10-03 11:10:10 +0700

    Bump up version to 1.6.21 Add grunt build files

--- dist/app/panels/bettermap/leaflet/leaflet.js
+++ dist/app/panels/bettermap/leaflet/leaflet.js
@@ -1,4 +1,4 @@
-/*! banana-fusion - v1.6.20 - 2018-06-18
+/*! banana-fusion - v1.6.21 - 2018-10-03
  * https://github.com/LucidWorks/banana/wiki
  * Copyright (c) 2018 Andrew Thanalertvisuti; Licensed Apache-2.0 */
 
