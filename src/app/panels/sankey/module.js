55e14c28b864c3b3fc49df26e176ae657bfa7343
Andrew Thanalertvisuti <andrew.thana@gmail.com>
2019-05-27 10:34:31 +0700

    Change DEBUG to false.

--- src/app/panels/sankey/module.js
+++ src/app/panels/sankey/module.js
@@ -46,7 +46,7 @@ define([
       };
 
       _.defaults($scope.panel, _d);
-      var DEBUG = true;
+      var DEBUG = false;
 
       $scope.init = function () {
         $scope.$on('refresh', function () {
