7560c25bacecd2ce9644858337d063c853a22d1c
Andrew Thanalertvisuti <andrew.thana@gmail.com>
2019-06-28 13:10:28 +0700

    Remove the double uri encoding in Filter service. (#343)

--- src/app/services/filterSrv.js
+++ src/app/services/filterSrv.js
@@ -240,11 +240,11 @@ define([
             }
           } else if (v.type === 'querystring') {
             if (v.mandate === 'must') {
-              filter_fq = filter_fq + '&fq=' + encodeURIComponent(v.query);
+              filter_fq = filter_fq + '&fq=' + v.query;
             } else if (v.mandate === 'mustNot') {
-              filter_fq = filter_fq + '&fq=-' + encodeURIComponent(v.query);
+              filter_fq = filter_fq + '&fq=-' + v.query;
             } else if (v.mandate === 'either') {
-              filter_either.push(encodeURIComponent(v.query));
+              filter_either.push(v.query);
             }
           } else if (v.type === 'range') {
             if (v.mandate === 'must') {
